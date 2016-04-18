a6a3553ff9d4186fe336c837906e1459d5333ccb
Josh DePaolo <pstrawberriedev@gmail.com>
2016-04-18 15:11:28 -0600

    updated

--- animation.js
+++ animation.js
@@ -9,23 +9,25 @@ $('#logo').on('click', function(e) {
   var cover = $('#emailInvasion .envelopeCover');
   var paper = $('#emailInvasion .paperForm');
   
+  TweenLite.set(cover, {transformStyle:"preserve-3d"});
+  
   //do things!
-  TweenLite.to(all, 0.35, {top:200,ease:Power1.easeOut, onComplete:coverFlip})
+  TweenLite.to(all, .8, {top:200,ease:Power1.easeOut, onComplete:coverFlip})
   
   //cover flip animation
-  function coverFlip() {
-    TweenLite.to(cover, 0.5, {transform:"rotateX(180deg)", top:"-115px",ease:Power1.easeOut, onComplete:envelopeOpen1})
+  function coverFlip() { //transform-origin:center top;
+    TweenLite.to(cover, 0.5, {transform:"rotateX(180deg)",ease:Power1.easeOut, onComplete:envelopeOpen1})
   }
   
   //envelope open animation
   function envelopeOpen1() {
-    TweenLite.to(paper, 0.25, {top:"-70px",height:15,autoAlpha:1,ease:Power1.easeOut,onComplete:envelopeOpen2})
+    TweenLite.to(paper, 0.25, {top:"-90px",height:15,autoAlpha:1, ease:Power1.easeOut,onComplete:envelopeOpen2})
   }
   function envelopeOpen2() {
-    TweenLite.to(paper, 0.25, {height:110, ease:Power1.easeOut,onComplete:envelopeOpen3})
+    TweenLite.to(paper, 0.25, {height:200, ease:Power1.easeOut,onComplete:envelopeOpen3})
   }
   function envelopeOpen3() {
-    TweenLite.to(paper, 0.25, {top:20,autoAlpha:1,ease:Power1.easeOut})
+    TweenLite.to(paper, 0.25, {top:"-65px",autoAlpha:1, overflow:"visible",ease:Power1.easeOut})
   }
   
 });
